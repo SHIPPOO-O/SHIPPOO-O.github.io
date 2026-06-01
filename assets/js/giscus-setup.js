404240a8635f3e6554affa89463b8b3ee6a9141a
SHIPPOO-O <hwanbum4056@gmail.com>
2026-06-01 05:34:13 +0000

    Deploying to gh-pages from @ SHIPPOO-O/SHIPPOO-O.github.io@61de72fd469ee3ecfbe909a76bfc332e4c28962d 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "SHIPPO-O/SHIPPO-O.github.io",
+    "data-repo": "SHIPPOO-O/SHIPPOO-O.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
